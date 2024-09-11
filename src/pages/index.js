2795ea0229a612523fd8be7765e2ae0afaf9fb00
your_github_username <garrettjamesprince@gmail.com>
2024-09-10 23:08:06 -0700

    cleaning up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,15 @@
 import Task from "@/components/Task";
-import Task3 from "@/components/Task3";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={`${inter.className} flex min-h-screen flex-col items-center`}>
-      <div className="">
-        <Task />
-      </div>
+    <main
+      className={`${inter.className} flex min-h-screen flex-col items-center`}
+    >
       <div className="pt-20">
-        <Task3 />
+        <Task />
       </div>
     </main>
   );
