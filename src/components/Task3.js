bf607e15d607f5e1f7c48ae201aaf0882ec14a3d
your_github_username <garrettjamesprince@gmail.com>
2024-09-10 22:04:32 -0700

    smaller styling changes

--- src/components/Task3.js
+++ src/components/Task3.js
@@ -112,7 +112,7 @@ function Task3() {
     //////////
     <motion.div
       layout
-      className="flex flex-col items-center justify-center w-[22.5rem] mx-auto"
+      className="flex flex-col items-center justify-center w-72 mx-auto font-medium"
     >
       <div className="flex w-full justify-end">
         <Button
@@ -246,7 +246,7 @@ function Task3() {
           <motion.div
             layout
             key={task.id}
-            className="task-card-shadow border-gray-300 border p-5 rounded-2xl mb-4"
+            className=" border-gray-300 border p-3 rounded-2xl mb-4"
           >
             {editingTaskId === task.id ? (
               <Formik
@@ -263,85 +263,79 @@ function Task3() {
                 }}
               >
                 {({ values, setFieldValue, isSubmitting, resetForm }) => (
-
-                    <Form>
-                      <Field
-                        as={Input}
-                        name="title"
-                        className="text-xl font-medium mb-4"
-                        autoComplete="off"
-                        placeholder="Task title"
-                        required
-                      />
-                      <Field
-                        as="textarea"
-                        name="description"
-                        className="w-full p-2 focus:border-gray-300 rounded-lg  focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent resize-none mb-5 transition-all duration-150 ease-in-out focus:ring-inset-2"
-                        rows={2}
-                        placeholder="Task description"
-                        autoComplete="off"
-                        required
-                        style={{ height: "auto", minHeight: "3rem" }}
-                        onInput={(e) => {
-                          e.target.style.height = "auto";
-                          e.target.style.height = e.target.scrollHeight + "px";
-                        }}
-                      />
-                      <Select
-                        layoutId="status"
-                        name="status"
-                        onValueChange={(value) => {
-                          setFieldValue("status", value);
-                          handleUpdate(task.id, "status", value);
-                        }}
-                        value={values.status}
-
-                        required
+                  <Form>
+                    <Field
+                      as={Input}
+                      name="title"
+                      className="text-xl font-medium mb-4"
+                      autoComplete="off"
+                      placeholder="Task title"
+                      required
+                    />
+                    <Field
+                      as="textarea"
+                      name="description"
+                      className="w-full p-2 focus:border-gray-300 rounded-lg  focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent resize-none mb-5 transition-all duration-150 ease-in-out focus:ring-inset-2 text-sm"
+                      rows={2}
+                      placeholder="Task description"
+                      autoComplete="off"
+                      required
+                      style={{ height: "auto", minHeight: "3rem" }}
+                      onInput={(e) => {
+                        e.target.style.height = "auto";
+                        e.target.style.height = e.target.scrollHeight + "px";
+                      }}
+                    />
+                    <Select
+                      layoutId="status"
+                      name="status"
+                      onValueChange={(value) => {
+                        setFieldValue("status", value);
+                        handleUpdate(task.id, "status", value);
+                      }}
+                      value={values.status}
+                      required
+                    >
+                      <SelectTrigger
+                        className={`transition-all ease-in-out duration-100 border rounded-lg px-2 py-1 flex items-center justify-between cursor-pointer h-7 mb-5  ${
+                          values.status === "Not Started"
+                            ? "bg-white hover:bg-gray-100 border-gray-200  focus:ring-gray-400"
+                            : values.status === "Research"
+                            ? "bg-purple-100 text-purple-600 hover:bg-purple-200/75 border-purple-200  focus:ring-purple-400"
+                            : values.status === "In Progress"
+                            ? "bg-blue-100 text-blue-600 hover:bg-blue-200/75 border-blue-200  focus:ring-blue-400"
+                            : values.status === "Stalled"
+                            ? "bg-orange-100 text-orange-600 hover:bg-orange-200/75 border-orange-200  focus:ring-orange-400"
+                            : "bg-green-100 text-green-600 hover:bg-green-200/75 border-green-200  focus:ring-green-400"
+                        } focus:outline-none focus:ring-2 focus:ring-offset-1`}
                       >
-                        <SelectTrigger
-                          className={`transition-all ease-in-out duration-100 border rounded-lg px-2 py-1 flex items-center justify-between cursor-pointer h-8 mb-5  ${
-                            values.status === "Not Started"
-                              ? "bg-white hover:bg-gray-100 border-gray-200  focus:ring-gray-400"
-                              : values.status === "Research"
-                              ? "bg-purple-100 text-purple-600 hover:bg-purple-200/75 border-purple-200  focus:ring-purple-400"
-                              : values.status === "In Progress"
-                              ? "bg-blue-100 text-blue-600 hover:bg-blue-200/75 border-blue-200  focus:ring-blue-400"
-                              : values.status === "Stalled"
-                              ? "bg-orange-100 text-orange-600 hover:bg-orange-200/75 border-orange-200  focus:ring-orange-400"
-                              : "bg-green-100 text-green-600 hover:bg-green-200/75 border-green-200  focus:ring-green-400"
-                          } focus:outline-none focus:ring-2 focus:ring-offset-1`}
-                        >
-                          <div className="flex items-center">
-                            <div
-                              className={`w-[0.35rem] h-[0.35rem] rounded-full mr-2 ${
-                                values.status === "Not Started"
-                                  ? "bg-gray-400"
-                                  : values.status === "Research"
-                                  ? "bg-purple-600"
-                                  : values.status === "In Progress"
-                                  ? "bg-blue-600"
-                                  : values.status === "Stalled"
-                                  ? "bg-orange-600"
-                                  : "bg-green-600"
-                              }`}
-                            ></div>
-                            <SelectValue placeholder="Status" />
-                          </div>
-                        </SelectTrigger>
-                        <SelectContent className="rounded-lg">
-                          <SelectItem value="Not Started">
-                            Not Started
-                          </SelectItem>
-                          <SelectItem value="Research">Research</SelectItem>
-                          <SelectItem value="In Progress">
-                            In Progress
-                          </SelectItem>
-                          <SelectItem value="Stalled">Stalled</SelectItem>
-                          <SelectItem value="Completed">Completed</SelectItem>
-                        </SelectContent>
-                      </Select>
-                      <div className="flex justify-between space-x-2">
-                        {/* <DatePicker
+                        <div className="flex items-center">
+                          <div
+                            className={`w-[0.35rem] h-[0.35rem] rounded-full mr-2 ${
+                              values.status === "Not Started"
+                                ? "bg-gray-400"
+                                : values.status === "Research"
+                                ? "bg-purple-600"
+                                : values.status === "In Progress"
+                                ? "bg-blue-600"
+                                : values.status === "Stalled"
+                                ? "bg-orange-600"
+                                : "bg-green-600"
+                            }`}
+                          ></div>
+                          <SelectValue placeholder="Status" />
+                        </div>
+                      </SelectTrigger>
+                      <SelectContent className="rounded-lg">
+                        <SelectItem value="Not Started">Not Started</SelectItem>
+                        <SelectItem value="Research">Research</SelectItem>
+                        <SelectItem value="In Progress">In Progress</SelectItem>
+                        <SelectItem value="Stalled">Stalled</SelectItem>
+                        <SelectItem value="Completed">Completed</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <div className="flex justify-between space-x-2">
+                      {/* <DatePicker
                             selected={values.dueDate}
                             onChange={(date) => {
                               setFieldValue("dueDate", date);
@@ -350,43 +344,43 @@ function Task3() {
                             className="min-w-[8rem]"
                             required
                           /> */}
-                        <Button
-                          type="button"
-                          className="w-20"
-                          variant="secondary"
-                          size="sm"
-                          onClick={() => {
-                            resetForm();
-                            setEditingTaskId(null);
-                          }}
-                        >
-                          Cancel
-                        </Button>
-                        <div className="flex justify-between space-x-2">
-                          <div>
-                            <Button
-                              type="button"
-                              className="w-20"
-                              variant="destructive"
-                              size="sm"
-                              onClick={() => handleDelete(task.id)}
-                            >
-                              <Trash2 className="w-3 h-3 mr-1 mb-[.1rem]" />
-                              <span>Delete</span>
-                            </Button>
-                          </div>
+                      <Button
+                        type="button"
+                        className="w-20"
+                        variant="secondary"
+                        size="sm"
+                        onClick={() => {
+                          resetForm();
+                          setEditingTaskId(null);
+                        }}
+                      >
+                        Cancel
+                      </Button>
+                      <div className="flex justify-between space-x-2">
+                        <div>
                           <Button
-                            type="submit"
+                            type="button"
                             className="w-20"
-                            variant="outline"
+                            variant="destructive"
                             size="sm"
-                            disabled={isSubmitting}
+                            onClick={() => handleDelete(task.id)}
                           >
-                            <Check className="w-3 h-3 mr-1 " />
-                            Save
+                            <Trash2 className="w-3 h-3 mr-1 mb-[.1rem]" />
+                            <span>Delete</span>
                           </Button>
                         </div>
+                        <Button
+                          type="submit"
+                          className="w-20"
+                          variant="outline"
+                          size="sm"
+                          disabled={isSubmitting}
+                        >
+                          <Check className="w-3 h-3 mr-1 " />
+                          Save
+                        </Button>
                       </div>
+                    </div>
                   </Form>
                 )}
               </Formik>
@@ -394,7 +388,7 @@ function Task3() {
               ///////////////////////////
               // Task List Non Editing //
               ///////////////////////////
-              <motion.div layout className="flex flex-col space-y-3">
+              <motion.div layout className="flex flex-col ">
                 {/* <div className="flex justify-end">
                   <button
                     onClick={() => setEditingTaskId(task.id)}
@@ -403,9 +397,9 @@ function Task3() {
                     <Pencil className="w-4 h-4" />
                   </button>
                 </div> */}
-                <h3 className="text-xl font-medium">{task.title}</h3>
-                <p>{task.description}</p>
-                <div className="flex justify-between space-x-4">
+                <h3 className="text-lg font-medium mb-2">{task.title}</h3>
+                <p className="text-sm mb-3">{task.description}</p>
+                <div className="flex justify-between items-center space-x-3">
                   {/* <DatePicker
                     selected={new Date(task.due_date)}
                     onChange={(date) => handleUpdate(task.id, "due_date", date)}
@@ -417,7 +411,7 @@ function Task3() {
                     }
                   >
                     <SelectTrigger
-                      className={`transition-all w-full ease-in-out duration-100 border rounded-lg px-2 py-1 flex items-center justify-between cursor-pointer h-8 ${
+                      className={`transition-all w-full ease-in-out duration-100 border rounded-lg px-2 py-1 flex items-center justify-between cursor-pointer h-6 font-medium ${
                         task.status === "Not Started"
                           ? "bg-white hover:bg-gray-100 border-gray-300 focus:ring-gray-400"
                           : task.status === "Research"
@@ -431,7 +425,7 @@ function Task3() {
                     >
                       <div className="flex items-center">
                         <div
-                          className={`w-[0.35rem] h-[0.35rem] rounded-full mr-2 ${
+                          className={`w-[0.35rem] h-[0.35rem] rounded-full mr-2 ml-1 ${
                             task.status === "Not Started"
                               ? "bg-gray-400"
                               : task.status === "Research"
@@ -456,9 +450,9 @@ function Task3() {
                   </Select>
                   <button
                     onClick={() => setEditingTaskId(task.id)}
-                    className="items-center text-gray-400 hover:text-black focus:text-black  hover:bg-gray-100 hover:shadow-sm p-2 rounded-lg transition-all ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-gray-400"
+                    className="items-center text-gray-400 hover:text-black focus:text-black  hover:bg-gray-100 hover:shadow-sm p-[.35rem] rounded-md transition-all ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-gray-400"
                   >
-                    <Pencil className="w-4 h-4" />
+                    <Pencil className="w-3 h-3" />
                   </button>
                 </div>
               </motion.div>
